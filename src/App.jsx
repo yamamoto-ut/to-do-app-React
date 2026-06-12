76e01322b0aeb22c44363b43bf2ad2e7dca60d3d
yamamoto <wdjtm423@gmail.com>
2026-06-12 23:50:30 +0900

    初回コミット

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+
+        <div>
+          <h1>Get started</h1>
+
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
