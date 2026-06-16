a1230f5417be4da998ed183076dcaa32f67615e5
yamamoto-ut <wdjtm423@gmail.com>
2026-06-16 17:35:55 +0900

    cssの一部コンポーネント化

--- src/components/inputTodo.jsx
+++ src/components/inputTodo.jsx
@@ -1,7 +1,17 @@
 export const InputTodo = (props) => {
 
-const {inputText, onChangeInputText, onClickAdd} = props;
+    const { inputText, onChangeInputText, onClickAdd } = props;
 
+    const style = {
+        backgroundColor: "#c6e5d9",
+        width: "400px",
+        height: "30px",
+        padding: "8px",
+        margin: "8px",
+        borderRadius: "8px"
+    }
+
+    
     return (
 
         <>
@@ -9,7 +19,7 @@ const {inputText, onChangeInputText, onClickAdd} = props;
                 <h1>Todo List</h1>
             </div>
 
-            <div className="input-area">
+            <div style={style}>
                 <input type="text" placeholder="Add a new todo" value={inputText} onChange={onChangeInputText} />
                 <button onClick={onClickAdd}>Add</button>
             </div>
